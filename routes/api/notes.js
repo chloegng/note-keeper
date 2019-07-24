cbf5d4528768e6f308166ed54abba0406c62637d
Chloe <chloegng@gmail.com>
2019-07-24 11:39:56 -0700

    get all notes

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -3,8 +3,21 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const Note = require('../../models/Notes');
 
+// @route     Get api/notes
+// @desc      Get notes route
+// @access    Public
+router.get('/', async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.json(notes);
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 // @route     POST api/notes
-// @desc      Test route
+// @desc      Add notes route
 // @access    Public
 router.post('/', [
   check('title', 'Title is required')
