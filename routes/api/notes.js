d1620482a4d46b6bcc811539090b3e334d27758c
Chloe <chloegng@gmail.com>
2019-07-23 18:44:49 -0700

    create add note route and validation

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route     GET api/notes
+// @route     POST api/notes
 // @desc      Test route
 // @access    Public
-router.get('/', (req, res) => res.send('notes route'));
+router.post('/', [
+  check('title', 'Title is required')
+    .not()
+    .isEmpty(),
+  check('body', 'Body is required')
+    .not()
+    .isEmpty()
+], (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  res.send('add notes route');
+});
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('hello, api is running!'))
+app.get('/', (req, res) => res.send('hello, api is running!'));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/notes', require('./routes/api/notes'));
