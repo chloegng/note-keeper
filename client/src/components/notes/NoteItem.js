98bd3ae018c8f7fa022cabb50fa1760518f810a1
Chloe <chloegng@gmail.com>
2019-07-24 22:45:21 -0700

    display data on NoteItem

--- client/src/components/notes/NoteItem.js
+++ client/src/components/notes/NoteItem.js
@@ -1,11 +1,18 @@
 import React, { Fragment } from 'react';
 
-const NoteItem = ({note: {title, body, _id }}) => {
+const NoteItem = ({note: {title, body, _id, dateCreated, dateUpdated }}) => {
 
   return (
     <Fragment>
-        <div className="card">
-          {title} {body} {_id}
+        <div className="card m-3">
+          <div className="card-header">
+            <h4>{title}</h4>
+          </div>
+          <div className="body m-3">
+            <p>{body}</p>
+            <small><strong>Date Created:</strong>{dateCreated}</small>
+            { dateUpdated !== dateCreated && <small><strong> Date Last Updated:</strong>{dateUpdated}</small> }
+          </div>
         </div>
     </Fragment>
   );
